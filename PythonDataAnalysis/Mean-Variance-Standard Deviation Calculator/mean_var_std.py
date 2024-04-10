dfa8c51e4cf218e961bd6ab582e535be315e8ac2
gkotzam <kgkotzam@csd.auth.gr>
2024-04-10 10:16:11 +0300

    adding calc function

---
+++ PythonDataAnalysis/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -0,0 +1,12 @@
+import numpy as np
+def calculate(value):
+	arr = np.array(value).reshape(3,3)
+	res= {
+		'mean': [list(np.mean(arr, axis=0)),list(np.mean(arr, axis=1)),arr.flatten().mean()],
+		'variance': [list(np.var(arr, axis=0)),list(np.var(arr, axis=1)),arr.flatten().var()],
+		'standard deviation': [list(np.std(arr, axis=0)),list(np.std(arr, axis=1)),arr.flatten().std()],
+		'max': [list(np.max(arr, axis=0)),list(np.max(arr, axis=1)),arr.flatten().max()],
+		'min': [list(np.min(arr, axis=0)),list(np.min(arr, axis=1)),arr.flatten().min()],
+		'sum': [list(np.sum(arr, axis=0)),list(np.sum(arr, axis=1)),arr.flatten().sum()]
+	}
+	print(res)
