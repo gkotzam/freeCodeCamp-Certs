86f722de3bc25a086054957a0b5ec0d52b38b8d4
gkotzam <kgkotzam@csd.auth.gr>
2024-11-22 01:09:33 +0200

    add static path

--- Back-End-Development-and-APIs/Basic Node and Express/myApp.js
+++ Back-End-Development-and-APIs/Basic Node and Express/myApp.js
@@ -1,10 +1,12 @@
 let express = require('express');
 let app = express();
+let path = require('path');
 
 
+app.use('/public', express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => {
-    res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html');
 })
 
 
