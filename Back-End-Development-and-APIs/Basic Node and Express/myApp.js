515c166c62e73513731fc569ba220e2e13c731f8
gkotzam <kgkotzam@csd.auth.gr>
2024-11-22 11:43:34 +0200

    .env test

--- Back-End-Development-and-APIs/Basic Node and Express/myApp.js
+++ Back-End-Development-and-APIs/Basic Node and Express/myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 let path = require('path');
-
+require('dotenv').config();
 
 app.use('/public', express.static(path.join(__dirname,'public')));
 
@@ -9,14 +9,19 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
 
-app.get('/json', (req, res, next) => {
-    res.json({message: 'Hello json'});
-});
+// app.get('/json', (req, res, next) => {
+//     res.json({message: 'Hello json'});
+// });
 
 
 
 
-
+app.get('/json', function(req, res) {
+    if (process.env.MESSAGE_STYLE=="uppercase") {
+    return res.json({ "message": "Hello json".toUpperCase()}) 
+    } else {
+    return res.json({ "message": "Hello json" })
+  }})
 
 
 
