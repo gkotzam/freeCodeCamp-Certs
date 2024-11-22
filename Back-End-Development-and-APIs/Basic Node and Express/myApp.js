535976f3997cb0035bf097806398b1af883aed1a
gkotzam <kgkotzam@csd.auth.gr>
2024-11-22 12:00:19 +0200

    /now => GET

--- Back-End-Development-and-APIs/Basic Node and Express/myApp.js
+++ Back-End-Development-and-APIs/Basic Node and Express/myApp.js
@@ -1,19 +1,24 @@
 let express = require('express');
-let app = express();
 let path = require('path');
 require('dotenv').config();
 
+
+let app = express();
+
 app.use('/public', express.static(path.join(__dirname,'public')));
+
+app.use( (req, res, next) => {
+    const method = req.method;
+    const path = req.path;
+    const ip = req.ip;
+    console.log(method + ' ' + path + ' - ' + ip);
+    next();
+});
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
-})
-
-// app.get('/json', (req, res, next) => {
-//     res.json({message: 'Hello json'});
-// });
-
-
+});
 
 
 app.get('/json', function(req, res) {
@@ -21,10 +26,15 @@ app.get('/json', function(req, res) {
     return res.json({ "message": "Hello json".toUpperCase()}) 
     } else {
     return res.json({ "message": "Hello json" })
-  }})
+  }});
 
 
-
+app.get('/now', function (req, res, next){
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({time: req.time});
+});
 
 
 
